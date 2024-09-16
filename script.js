ab385a74434c472ceef65a0fe0bde698183f5db0
GufronBoy <ultramannnboy@gmail.com>
2024-09-16 08:42:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("YaBtn").onclick = function () {
+  alert(
+    "-Kesuksesan adalah hasil dari kerja keras dan ketekunan -Keberhasilan adalah milik mereka yang berani mencoba -Jangan takut gagal, karena kegagalan adalah awal dari kesuksesan -Hanya orang-orang bodoh yang gampang puas -Kerja keras hari ini adalah kesuksesan di masa depan"
+  );
+};
+document.getElementById("TidakBtn").onclick = function () {
+  aler(```Ngapain milih "Tidak bang???"```);
+};
